dae2a525c5481de9493864bc67954cd113d58e5e
Tiago <tiagojacintodev@gmail.com>
2024-07-18 12:46:47 +0100

    change form handling

--- frontend/src/pages/login/page.tsx
+++ frontend/src/pages/login/page.tsx
@@ -1,58 +1,52 @@
-import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+
+type LoginFormValues = {
+  email: string;
+  password: string;
+};
 
 export default function Login() {
   const navigateTo = useNavigate();
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
   });
+
+  const onSubmit: SubmitHandler<LoginFormValues> = async (data) => {
+    const response = await fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) throw new Error('Invalid credentials');
+
+    const { userId } = await response.json();
+
+    navigateTo(`/choose-company?userId=${userId}`);
+  };
 
   return (
     <div className='py-6 px-36'>
       <h1 className='text-xl'>Login</h1>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-
-          Object.values(formData).forEach((v) => {
-            if (!v) {
-              throw new Error('All fields are required');
-            }
-          });
-
-          const response = await fetch('http://localhost:3000/api/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-          });
-
-          if (!response.ok) throw new Error('Invalid credentials');
-
-          const { userId } = await response.json();
-
-          navigateTo(`/choose-company?userId=${userId}`);
-        }}
-        className='flex flex-col gap-4 mt-5'
-      >
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 mt-5'>
         <label>Email</label>
         <input
           type='email'
-          name='email'
           className='border border-black rounded-lg h-9 pl-2.5'
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          {...register('email')}
         />
 
         <label>Password</label>
         <input
           type='password'
-          name='password'
           className='border border-black rounded-lg h-9 pl-2.5'
-          value={formData.password}
-          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+          {...register('password')}
         />
 
         <button type='submit' className='bg-black text-white h-11 rounded-lg w-64 self-center mt-3'>
--- frontend/src/pages/signup/page.tsx
+++ frontend/src/pages/signup/page.tsx
@@ -1,99 +1,87 @@
-import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+
+type SignupFormValues = {
+  country: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  companyName: string;
+};
 
 export default function Signup() {
   const navigateTo = useNavigate();
-  const [formData, setFormData] = useState({
-    country: 'portugal',
-    email: 't@t.t',
-    firstName: 't',
-    lastName: 't',
-    password: 't',
-    companyName: 't',
+
+  const { register, handleSubmit } = useForm<SignupFormValues>({
+    defaultValues: {
+      country: 'portugal',
+      email: '',
+      firstName: '',
+      lastName: '',
+      password: '',
+      companyName: '',
+    },
   });
+
+  const onSubmit: SubmitHandler<SignupFormValues> = async (data) => {
+    const response = await fetch('http://localhost:3000/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) throw new Error('Invalid credentials');
+
+    const { companyName } = await response.json();
+
+    navigateTo(`dashboard?company=${companyName}`);
+  };
 
   return (
     <div className='py-6 px-36'>
       <h1 className='text-xl'>Signup</h1>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-
-          Object.values(formData).forEach((v) => {
-            if (!v) {
-              throw new Error('All fields are required');
-            }
-          });
-
-          const response = await fetch('http://localhost:3000/api/signup', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-          });
-
-          if (!response.ok) throw new Error('Invalid credentials');
-
-          const { companyName } = await response.json();
-
-          navigateTo(`dashboard?company=${companyName}`);
-        }}
-        className='flex flex-col gap-4 mt-5'
-      >
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 mt-5'>
         <label>Company Name</label>
         <input
           type='text'
-          name='companyName'
           className='border border-black rounded-lg h-9 pl-2.5'
-          value={formData.companyName}
-          onChange={(e) => setFormData({ ...formData, companyName: e.target.value })}
+          {...register('companyName')}
         />
 
         <label>First Name</label>
         <input
           type='text'
-          name='firstName'
           className='border border-black rounded-lg h-9 pl-2.5'
-          value={formData.firstName}
-          onChange={(e) => setFormData({ ...formData, firstName: e.target.value })}
+          {...register('firstName')}
         />
 
         <label>Last Name</label>
         <input
           type='text'
-          name='lastName'
           className='border border-black rounded-lg h-9 pl-2.5'
-          value={formData.lastName}
-          onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
+          {...register('lastName')}
         />
 
         <label>Country</label>
-        <select
-          name='country'
-          className='border border-black rounded-lg h-9 pl-2.5'
-          value={formData.country}
-          onChange={(e) => setFormData({ ...formData, country: e.target.value })}
-        >
+        <select className='border border-black rounded-lg h-9 pl-2.5' {...register('country')}>
           <option value='portugal'>Portugal</option>
         </select>
 
         <label>Email</label>
         <input
           type='email'
-          name='email'
           className='border border-black rounded-lg h-9 pl-2.5'
-          value={formData.email}
-          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          {...register('email')}
         />
 
         <label>Password</label>
         <input
           type='password'
-          name='password'
           className='border border-black rounded-lg h-9 pl-2.5'
-          value={formData.password}
-          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+          {...register('password')}
         />
 
         <button type='submit' className='bg-black text-white h-11 rounded-lg w-64 self-center mt-3'>
